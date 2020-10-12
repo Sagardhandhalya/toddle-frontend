45c66cfa621c3ee6a2431252a76c5d60d22ee7da
sagar dhandhalya <dhandhalyasagar0@gmail.com>
2020-10-12 23:11:25 +0530

    Updates

---
+++ precache-manifest.6013b5ef528e0689f63d651a43aded38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddd7907ee54f36f9d0ba54f8535f2b0",
+    "url": "/toddlecat/index.html"
+  },
+  {
+    "revision": "c42947415bf8f575e852",
+    "url": "/toddlecat/static/css/main.fd1f1ab7.chunk.css"
+  },
+  {
+    "revision": "71820b2c8989d948622c",
+    "url": "/toddlecat/static/js/2.c88e9e56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toddlecat/static/js/2.c88e9e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42947415bf8f575e852",
+    "url": "/toddlecat/static/js/main.dd3c7e3d.chunk.js"
+  },
+  {
+    "revision": "94dc190a9ccac34852a4",
+    "url": "/toddlecat/static/js/runtime-main.cbcfe3ee.js"
+  }
+]);
